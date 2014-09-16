7b850b9a8f0cca29068baa924c35a0bfba1d6650
wuzesheng <wuzesheng@xiaomi.com>
2014-09-16 19:26:29 +0800

    Merge HDFS-6294: Use INode IDs to avoid conflicts when a file open for write is renamed
    Summary: Ref T1896
    
    Test Plan: N/A
    
    Reviewers: peng.zhang, xieliang
    
    Reviewed By: xieliang
    
    Subscribers: infra-dev
    
    Maniphest Tasks: T1896
    
    Differential Revision: https://phabricator.d.xiaomi.net/D6253

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSImage.java
@@ -541,7 +541,7 @@ public class FSImage implements Closeable {
   }
 
   @VisibleForTesting
-  void setEditLogForTesting(FSEditLog newLog) {
+  public void setEditLogForTesting(FSEditLog newLog) {
     editLog = newLog;
   }
 
