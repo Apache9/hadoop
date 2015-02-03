2a06c1038c01b2403e5224d024acff7de033ddf4
peng.zhang <peng.zhang@xiaomi.com>
2015-02-03 21:34:05 +0800

    Revert "redirect stdout stderr to /dev/null"
    This reverts commit c52ec019289ec6f9864c662feb1337f0cb118ffe.

--- hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/main/java/org/apache/hadoop/mapred/YARNRunner.java
+++ hadoop-mapreduce-project/hadoop-mapreduce-client/hadoop-mapreduce-client-jobclient/src/main/java/org/apache/hadoop/mapred/YARNRunner.java
@@ -425,8 +425,10 @@ public class YARNRunner implements ClientProtocol {
     vargs.add(mrAppMasterUserOptions);
     
     vargs.add(MRJobConfig.APPLICATION_MASTER_CLASS);
-    vargs.add("1>/dev/null");
-    vargs.add("2>/dev/null");
+    vargs.add("1>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR +
+        Path.SEPARATOR + ApplicationConstants.STDOUT);
+    vargs.add("2>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR +
+        Path.SEPARATOR + ApplicationConstants.STDERR);
 
 
     Vector<String> vargsFinal = new Vector<String>(8);
