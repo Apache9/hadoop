9928afc55d4e99542728d01d1c7d2c254ef43d7a
xieliang <xieliang@xiaomi.com>
2015-04-21 17:18:27 +0800

    fix the DDos namenode issue due to DFSClient side infinite loop
    Summary: Fixes T3714
    
    Test Plan: TestPread
    
    Reviewers: liushaohui, heliangliang, zhouyingchao
    
    Reviewed By: zhouyingchao
    
    Maniphest Tasks: T3714
    
    Differential Revision: https://phabricator.d.xiaomi.net/D12821

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/DFSInputStream.java
@@ -17,6 +17,7 @@
  */
 package org.apache.hadoop.hdfs;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
@@ -1044,6 +1045,11 @@ implements ByteBufferReadable, CanSetDropBehind, CanSetReadahead,
         actualGetFromOneDataNode(addressPair, block, start, end, buf, offset,
             corruptedBlockMap);
         return;
+      } catch (FileNotFoundException fnfe) {
+        // see T3714. Through we can throw exception here directly maybe.
+        // for safety, let's play with deadNode&retry:)
+        DFSClient.LOG.warn("", fnfe);
+        addToDeadNodes(addressPair.info);
       } catch (IOException e) {
         // Ignore. Already processed inside the function.
         // Loop through to try the next node.
