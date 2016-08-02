41f3636146e87471120f5612dfae0ac96f063202
zhangduo <zhangduo@apache.org>
2016-08-02 17:36:55 +0800

    small optimization of acl
    Summary: remove duplicated getAclFeature call
    
    Test Plan: N/A
    
    Reviewers: zhangguanghao1, yangzhe5, huzheng, zhangchen5
    
    Reviewed By: zhangchen5
    
    Subscribers: infra-dev
    
    Differential Revision: https://phabricator.d.xiaomi.net/D37761
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeWithAdditionalFields.java

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeDirectory.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/INodeDirectory.java
@@ -784,8 +784,9 @@ public class INodeDirectory extends INodeWithAdditionalFields
     for (INode child : getChildrenList(Snapshot.CURRENT_STATE_ID)) {
       child.destroyAndCollectBlocks(collectedBlocks, removedINodes);
     }
-    if (getAclFeature() != null) {
-      AclStorage.removeAclFeature(getAclFeature());
+    AclFeature aclFeature = getAclFeature();
+    if (aclFeature != null) {
+      AclStorage.removeAclFeature(aclFeature);
     }
     clear();
     removedINodes.add(this);
