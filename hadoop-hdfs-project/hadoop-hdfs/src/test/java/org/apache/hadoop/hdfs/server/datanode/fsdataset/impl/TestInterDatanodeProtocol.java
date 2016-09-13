3732453e3c54534b2cfe1fdce274789ca273138e
zhouyingchao <zhouyingchao@xiaomi.com>
2016-09-13 15:24:15 +0800

    Optimize stopWriter to prevent live lock
    Summary: Ref T7071
    
    Test Plan: unit test
    
    Reviewers: zhangchen5
    
    Reviewed By: zhangchen5
    
    Maniphest Tasks: T7071
    
    Differential Revision: https://phabricator.d.xiaomi.net/D40233
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/FsDatasetImpl.java

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/TestInterDatanodeProtocol.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/fsdataset/impl/TestInterDatanodeProtocol.java
@@ -233,7 +233,14 @@ public class TestInterDatanodeProtocol {
     final long gs = 7777L;
     final long length = 22L;
     final ReplicaMap map = new ReplicaMap(this);
+    MiniDFSCluster cluster = new MiniDFSCluster.Builder(new HdfsConfiguration()).build();
+    FsDatasetImpl dataSet;
     String bpid = "BP-TEST";
+    
+    try {
+    cluster.waitActive();
+    DataNode dn = cluster.getDataNodes().get(0);
+    dataSet = (FsDatasetImpl)DataNodeTestUtils.getFSDataset(dn);
     final Block[] blocks = new Block[5];
     for(int i = 0; i < blocks.length; i++) {
       blocks[i] = new Block(firstblockid + i, length, gs);
@@ -241,77 +248,85 @@ public class TestInterDatanodeProtocol {
     }
     
     { 
-      //normal case
-      final Block b = blocks[0];
-      final ReplicaInfo originalInfo = map.get(bpid, b);
+        // normal case
+        final Block b = blocks[0];
+        final ReplicaInfo originalInfo = map.get(bpid, b);
 
-      final long recoveryid = gs + 1;
-      final ReplicaRecoveryInfo recoveryInfo = FsDatasetImpl
-          .initReplicaRecovery(bpid, map, blocks[0], recoveryid,
-              DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
-      assertEquals(originalInfo, recoveryInfo);
+        final long recoveryid = gs + 1;
+        final ReplicaRecoveryInfo recoveryInfo =
+            dataSet.initReplicaRecovery(bpid, map, blocks[0], recoveryid,
+                DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
+        assertEquals(originalInfo, recoveryInfo);
 
-      final ReplicaUnderRecovery updatedInfo = (ReplicaUnderRecovery)map.get(bpid, b);
-      Assert.assertEquals(originalInfo.getBlockId(), updatedInfo.getBlockId());
-      Assert.assertEquals(recoveryid, updatedInfo.getRecoveryID());
+        final ReplicaUnderRecovery updatedInfo =
+            (ReplicaUnderRecovery) map.get(bpid, b);
+        Assert
+            .assertEquals(originalInfo.getBlockId(), updatedInfo.getBlockId());
+        Assert.assertEquals(recoveryid, updatedInfo.getRecoveryID());
 
-      //recover one more time 
-      final long recoveryid2 = gs + 2;
-      final ReplicaRecoveryInfo recoveryInfo2 = FsDatasetImpl
+        // recover one more time
+        final long recoveryid2 = gs + 2;
+        final ReplicaRecoveryInfo recoveryInfo2 =
+            dataSet
           .initReplicaRecovery(bpid, map, blocks[0], recoveryid2,
               DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
       assertEquals(originalInfo, recoveryInfo2);
 
-      final ReplicaUnderRecovery updatedInfo2 = (ReplicaUnderRecovery)map.get(bpid, b);
-      Assert.assertEquals(originalInfo.getBlockId(), updatedInfo2.getBlockId());
-      Assert.assertEquals(recoveryid2, updatedInfo2.getRecoveryID());
+        final ReplicaUnderRecovery updatedInfo2 =
+            (ReplicaUnderRecovery) map.get(bpid, b);
+        Assert.assertEquals(originalInfo.getBlockId(),
+            updatedInfo2.getBlockId());
+        Assert.assertEquals(recoveryid2, updatedInfo2.getRecoveryID());
       
-      //case RecoveryInProgressException
-      try {
-        FsDatasetImpl.initReplicaRecovery(bpid, map, b, recoveryid,
-            DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
-        Assert.fail();
+        // case RecoveryInProgressException
+        try {
+          dataSet.initReplicaRecovery(bpid, map, b, recoveryid,
+              DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
+          Assert.fail();
+        } catch (RecoveryInProgressException ripe) {
+          System.out.println("GOOD: getting " + ripe);
+        }
       }
-      catch(RecoveryInProgressException ripe) {
-        System.out.println("GOOD: getting " + ripe);
-      }
-    }
 
-    { // BlockRecoveryFI_01: replica not found
-      final long recoveryid = gs + 1;
-      final Block b = new Block(firstblockid - 1, length, gs);
-      ReplicaRecoveryInfo r = FsDatasetImpl.initReplicaRecovery(bpid, map, b,
-          recoveryid,
-          DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
-      Assert.assertNull("Data-node should not have this replica.", r);
-    }
+      { // BlockRecoveryFI_01: replica not found
+        final long recoveryid = gs + 1;
+        final Block b = new Block(firstblockid - 1, length, gs);
+        ReplicaRecoveryInfo r =
+            dataSet.initReplicaRecovery(bpid, map, b, recoveryid,
+                DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
+        Assert.assertNull("Data-node should not have this replica.", r);
+      }
     
-    { // BlockRecoveryFI_02: "THIS IS NOT SUPPOSED TO HAPPEN" with recovery id < gs  
-      final long recoveryid = gs - 1;
-      final Block b = new Block(firstblockid + 1, length, gs);
-      try {
-        FsDatasetImpl.initReplicaRecovery(bpid, map, b, recoveryid,
-            DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
-        Assert.fail();
+      { // BlockRecoveryFI_02: "THIS IS NOT SUPPOSED TO HAPPEN" with recovery id
+        // < gs
+        final long recoveryid = gs - 1;
+        final Block b = new Block(firstblockid + 1, length, gs);
+        try {
+          dataSet.initReplicaRecovery(bpid, map, b, recoveryid,
+              DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
+          Assert.fail();
+        } catch (IOException ioe) {
+          System.out.println("GOOD: getting " + ioe);
+        }
       }
-      catch(IOException ioe) {
-        System.out.println("GOOD: getting " + ioe);
-      }
-    }
 
-    // BlockRecoveryFI_03: Replica's gs is less than the block's gs
-    {
-      final long recoveryid = gs + 1;
-      final Block b = new Block(firstblockid, length, gs+1);
-      try {
-        FsDatasetImpl.initReplicaRecovery(bpid, map, b, recoveryid,
-            DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
-        fail("InitReplicaRecovery should fail because replica's " +
-        		"gs is less than the block's gs");
-      } catch (IOException e) {
-        e.getMessage().startsWith(
-           "replica.getGenerationStamp() < block.getGenerationStamp(), block=");
+      // BlockRecoveryFI_03: Replica's gs is less than the block's gs
+      {
+        final long recoveryid = gs + 1;
+        final Block b = new Block(firstblockid, length, gs + 1);
+        try {
+          dataSet.initReplicaRecovery(bpid, map, b, recoveryid,
+              DFSConfigKeys.DFS_DATANODE_XCEIVER_STOP_TIMEOUT_MILLIS_DEFAULT);
+          fail("InitReplicaRecovery should fail because replica's "
+              + "gs is less than the block's gs");
+        } catch (IOException e) {
+          e.getMessage()
+              .startsWith(
+                  "replica.getGenerationStamp() < block.getGenerationStamp(), block=");
+        }
       }
+    } finally {
+      cluster.shutdown();
     }
   }
 
