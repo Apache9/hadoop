bc6eecb39356cc8fba9eb2209c30165b99e3cebc
zhangchen <zhangchen5@xiaomi.com>
2016-09-19 13:49:03 +0800

    backport HDFS-7097, Allow block reports to be processed during checkpointing on standby name node
    Summary: Ref T6858
    
    Test Plan: UT
    
    Reviewers: zhangxiaotong3, zhouyingchao
    
    Reviewed By: zhouyingchao
    
    Maniphest Tasks: T6858
    
    Differential Revision: https://phabricator.d.xiaomi.net/D40737
    
    Conflicts:
    	hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/FSNamesystem.java
@@ -528,6 +528,15 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
   /** Lock to protect FSNamesystem. */
   private final FSNamesystemLock fsLock;
 
+  /** 
+   * Checkpoint lock to protect FSNamesystem modification on standby NNs.
+   * Unlike fsLock, it does not affect block updates. On active NNs, this lock
+   * does not provide proper protection, because there are operations that
+   * modify both block and name system state.  Even on standby, fsLock is 
+   * used when block state changes need to be blocked.
+   */
+  private final ReentrantLock cpLock;
+
   /**
    * Used when this NN is in standby state to read from the shared edit log.
    */
@@ -808,6 +817,8 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
     LOG.info("fsLock is fair:" + fair);
     fsLock = new FSNamesystemLock(fair);
     cond = fsLock.writeLock().newCondition();
+    cpLock = new ReentrantLock();
+
     this.fsImage = fsImage;
     try {
       resourceRecheckInterval = conf.getLong(
@@ -1593,6 +1604,22 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
   public int getWriteHoldCount() {
     return this.fsLock.getWriteHoldCount();
   }
+
+  /** Lock the checkpoint lock */
+  public void cpLock() {
+    this.cpLock.lock();
+  }
+
+  /** Lock the checkpoint lock interrupibly */
+  public void cpLockInterruptibly() throws InterruptedException {
+    this.cpLock.lockInterruptibly();
+  }
+
+  /** Unlock the checkpoint lock */
+  public void cpUnlock() {
+    this.cpLock.unlock();
+  }
+    
 
   NamespaceInfo getNamespaceInfo() {
     readLock();
@@ -5742,6 +5769,8 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
       return; // Return previous response
     }
     boolean success = false;
+
+    cpLock();  // Block if a checkpointing is in progress on standby.
     readLock();
     try {
       checkOperation(OperationCategory.UNCHECKED);
@@ -5754,6 +5783,7 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
       success = true;
     } finally {
       readUnlock();
+      cpUnlock();
       RetryCache.setState(cacheEntry, success);
     }
     LOG.info("New namespace image has been created");
@@ -5769,6 +5799,7 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
       StandbyException {
     checkSuperuserPrivilege();
     checkOperation(OperationCategory.UNCHECKED);
+    cpLock();  // Block if a checkpointing is in progress on standby.
     writeLock();
     try {
       checkOperation(OperationCategory.UNCHECKED);
@@ -5783,6 +5814,7 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
       return val;
     } finally {
       writeUnlock();
+      cpUnlock();
     }
   }
 
@@ -5793,12 +5825,14 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
   void finalizeUpgrade() throws IOException {
     checkSuperuserPrivilege();
     checkOperation(OperationCategory.UNCHECKED);
+    cpLock();  // Block if a checkpointing is in progress on standby.
     writeLock();
     try {
       checkOperation(OperationCategory.UNCHECKED);
       getFSImage().finalizeUpgrade(this.isHaEnabled() && inActiveState());
     } finally {
       writeUnlock();
+      cpUnlock();
     }
   }
 
@@ -8205,6 +8239,11 @@ public class FSNamesystem implements Namesystem, FSClusterStats,
   @VisibleForTesting
   public ReentrantLock getLongReadLockForTests() {
     return fsLock.longReadLock;
+  }
+
+  @VisibleForTesting
+  public ReentrantLock getCpLockForTests() {
+    return cpLock;
   }
 
   @VisibleForTesting
