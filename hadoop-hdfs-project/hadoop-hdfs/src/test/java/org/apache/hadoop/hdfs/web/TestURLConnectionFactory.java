8f4ae0c9fc076830d9b9629b216481312a336feb
zhangbiao <zhangbiao1@xiaomi.com>
2016-10-12 18:51:47 +0800

    Backport HDFS-7798(https://issues.apache.org/jira/browse/HDFS-7798): Fix checkpointing failure caused by shared KerberosAuthenticator
    Summary: Ref T7235
    
    Test Plan: Unit Test
    
    Reviewers: zhangchen5, zhouyingchao
    
    Reviewed By: zhouyingchao
    
    Subscribers: infra-dev
    
    Maniphest Tasks: T7235
    
    Differential Revision: https://phabricator.d.xiaomi.net/D41797

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/web/TestURLConnectionFactory.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/web/TestURLConnectionFactory.java
@@ -18,20 +18,26 @@
 package org.apache.hadoop.hdfs.web;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.security.authentication.client.AuthenticationException;
 import org.apache.hadoop.security.authentication.client.ConnectionConfigurator;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hdfs.MiniDFSCluster;
 import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.common.collect.Lists;
 
+
 public final class TestURLConnectionFactory {
 
+  public static final Log LOG = LogFactory.getLog(TestURLConnectionFactory.class);
   @Test
-  public void testConnConfiguratior() throws IOException {
+  public void testConnConfiguratior() throws IOException, AuthenticationException {
     final URL u = new URL("http://localhost");
     final List<HttpURLConnection> conns = Lists.newArrayList();
     URLConnectionFactory fc = new URLConnectionFactory(new ConnectionConfigurator() {
@@ -47,4 +53,52 @@ public final class TestURLConnectionFactory {
     fc.openConnection(u);
     Assert.assertEquals(1, conns.size());
   }
+
+  public static final int CONNECT_TIMES = 1000;
+  private static class OpenConnectionThread extends Thread {
+    private URL mURL;
+    private URLConnectionFactory mUCF;
+    public static int sSuccessCount = 0;
+    public OpenConnectionThread(Configuration conf, URL url) {
+      mUCF = URLConnectionFactory.newDefaultURLConnectionFactory(conf);
+      mURL = url;
+    }
+    public void run() {
+      for (int i = 0; i < CONNECT_TIMES; ++i) {
+        try {
+          mUCF.openConnection(mURL, true);
+          ++sSuccessCount;
+        }
+        catch(AuthenticationException e){
+          LOG.info("UnExpected - AuthenticationException caught", e);
+        } catch(ConnectException e){
+          //Do nothing here to speed up execution
+        } catch(IOException e){
+          LOG.info("UnExpected - IOException caught", e);
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testForIssueHDFS7798()
+      throws IOException, AuthenticationException, URISyntaxException, InterruptedException {
+
+    //URL1 with which the openConnection() will succeed
+    Configuration conf = new Configuration();
+    MiniDFSCluster cluster = new MiniDFSCluster.Builder(conf).build();
+    URI uri = new URI(cluster.getHttpUri(0));
+
+    //URL2 with which the openConnection() will raise java.net.ConnectException
+    URL url = new URL("http://localhost");
+    OpenConnectionThread t1 = new OpenConnectionThread(conf, uri.toURL());
+    OpenConnectionThread t2 = new OpenConnectionThread(conf, url);
+    t1.start();
+    t2.start();
+    t1.join();
+    t2.join();
+
+    Assert.assertEquals(OpenConnectionThread.sSuccessCount, CONNECT_TIMES);
+  }
 }
+
