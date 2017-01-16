b609687531af995f9315eac98c5f470fd526c383
Xie Gang <xiegang1@xiaomi.com>
2017-01-16 19:26:43 +0800

    Exclude the dead node when monitor the availability of the datanode
    Summary: Ref T8054
    
    Test Plan: UT
    
    Reviewers: zhangchen5, zhouyingchao
    
    Reviewed By: zhouyingchao
    
    Maniphest Tasks: T8054
    
    Differential Revision: https://phabricator.d.xiaomi.net/D48803

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/Canary.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/tools/Canary.java
@@ -199,6 +199,68 @@ public class Canary implements Tool {
       }
     }
 
+    private ArrayList<String> getDeadDatanodes() {
+
+      try {
+
+        List<DFSUtil.ConfiguredNNAddress> nns =
+            DFSUtil.flattenAddressMap(DFSUtil.getNNServiceRpcAddresses(conf));
+
+        if (nns.size() <= 0) {
+          LOG.error("could not get namenode when getting dead datanodes");
+          return null;
+        }
+        // choose any of the nn should be OK
+        DFSUtil.ConfiguredNNAddress cnn = nns.get(0);
+        InetSocketAddress isa = cnn.getAddress();
+        URL nnJmxUrl = new URL(DFSUtil.getInfoServer(isa, conf,
+              DFSUtil.getHttpClientScheme(conf)).toURL(), NN_JMX_SUFFIX);
+        String jmxJson = null;
+        try {
+          ArrayList<String> deadnodesList = new ArrayList<String>();
+          jmxJson = readOutput(nnJmxUrl);
+          JSONObject jsonObj = new JSONObject(jmxJson);
+          String deadNodesStr = (String) jsonObj.getJSONArray("beans").getJSONObject(0).get("DeadNodes");
+          JSONObject deadNodesJO = new JSONObject(deadNodesStr);
+          if (deadNodesJO != null && deadNodesJO.length() > 0) {
+            JSONArray ja = deadNodesJO.names();
+            for (int i = 0; i < deadNodesJO.length(); i ++) {
+              String xferaddrStr = deadNodesJO.getJSONObject(ja.getString(i)).getString("xferaddr");
+              deadnodesList.add(xferaddrStr);
+
+            }
+          }
+          return deadnodesList;
+
+        } catch (IOException ioe) {
+          LOG.error("Get JMX from NameNode faild, jmx url:" + nnJmxUrl, ioe);
+        } catch (JSONException je) {
+          if (jmxJson != null)
+            LOG.error("Can't parse the jmx content as JSON\n" + jmxJson);
+        }
+
+      } catch (IOException e) {
+        LOG.error("getting dead datanode failed", e);
+      }
+
+      return null;
+
+    }
+
+    private void removeDeadNodesFromTaskList(ArrayList<DatanodeInfo> tasklist, ArrayList<String> datanodes) {
+      if (tasklist == null || datanodes == null) {
+        return;
+      }
+      for (int i = 0; i < datanodes.size(); i ++) {
+        for (int j =0 ; j < tasklist.size(); j ++) {
+          if (datanodes.get(i).equals(tasklist.get(j).getXferAddr())) {
+            tasklist.remove(j);
+            break;
+          }
+        }
+      }
+    }
+
     public synchronized void initTasks () {
       // we need ensure all the nodes is probed before refresh the task list
       if (!isTaskCompleted) {
@@ -206,9 +268,14 @@ public class Canary implements Tool {
       }
       try {
         taskList = new ArrayList<DatanodeInfo> (Arrays.asList(dfs.getDataNodeStats()));
-          isTaskCompleted = false;
-          taskNum = taskList.size();
-          currTask = 0;
+        // remove dead node
+        ArrayList deadNodeList = getDeadDatanodes();
+        if (deadNodeList != null && deadNodeList.size() > 0) {
+          removeDeadNodesFromTaskList(taskList, deadNodeList);
+        }
+        isTaskCompleted = false;
+        taskNum = taskList.size();
+        currTask = 0;
       } catch (IOException e) {
         LOG.error("Init prob tasks failed", e);
       }
