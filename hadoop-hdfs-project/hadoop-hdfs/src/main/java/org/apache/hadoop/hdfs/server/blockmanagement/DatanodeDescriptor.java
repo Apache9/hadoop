8a38393ce70f437a04112a7c9a89643d1e2f43ac
Xie Gang <xiegang1@xiaomi.com>
2017-07-21 21:04:38 +0800

    fix the issue of only one volume is picked when getBlocks
    Summary: Ref T8756
    
    Test Plan: UT
    
    Reviewers: zhouyingchao, zhangchen5, yangyun1
    
    Reviewed By: zhangchen5
    
    Maniphest Tasks: T8756
    
    Differential Revision: https://phabricator.d.xiaomi.net/D62724

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeDescriptor.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/blockmanagement/DatanodeDescriptor.java
@@ -493,6 +493,9 @@ public class DatanodeDescriptor extends DatanodeInfo {
   Iterator<BlockInfo> getBlockIterator(final String storageID) {
     return new BlockIterator(getStorageInfo(storageID));
   }
+  Iterator<BlockInfo> getBlockIterator(final DatanodeStorageInfo[] datanodeStorageInfos) {
+    return new BlockIterator(datanodeStorageInfos);
+  }
 
   /**
    * Store block replication work.
