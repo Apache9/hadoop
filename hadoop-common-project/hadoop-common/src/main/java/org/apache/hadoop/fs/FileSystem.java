b21bd9e7d152ccfa2489b383319f544ebc31f8b9
lijinglun [李经纶] <lijinglun@xiaomi.com>
2017-10-19 17:18:12 +0800

    ConfigurationService supports RegisterCenter(NameService)
    Summary:
    Ref T9230
    Now ConfigurationService supports getting conf from both zk and RegisterCenter
    
    Test Plan: UT
    
    Reviewers: zhangchen5, zhouyingchao, xiegang1, yangyun1, zhangsen
    
    Reviewed By: zhangchen5
    
    Maniphest Tasks: T9230
    
    Differential Revision: https://phabricator.d.xiaomi.net/D69620

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/FileSystem.java
@@ -21,7 +21,6 @@ import java.io.Closeable;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.lang.ref.WeakReference;
-import java.lang.reflect.Constructor;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.security.PrivilegedExceptionAction;
@@ -35,7 +34,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.NoSuchElementException;
-import java.util.Properties;
 import java.util.ServiceLoader;
 import java.util.Set;
 import java.util.Stack;
@@ -341,32 +339,31 @@ public abstract class FileSystem extends Configured implements Closeable {
     return (LocalFileSystem)get(LocalFileSystem.NAME, conf);
   }
 
-  /** Returns the FileSystem for this URI's scheme and authority.  The scheme
-   * of the URI determines a configuration property name,
-   * <tt>fs.<i>scheme</i>.class</tt> whose value names the FileSystem class.
-   * The entire URI is passed to the FileSystem instance's initialize method.
+  /**
+   * Returns the FileSystem for this URI's scheme and authority. The scheme of
+   * the URI determines a configuration property name,
+   * <tt>fs.<i>scheme</i>.class</tt> whose value names the FileSystem class. The
+   * entire URI is passed to the FileSystem instance's initialize method.
    */
   public static FileSystem get(URI uri, Configuration conf) throws IOException {
     String scheme = uri.getScheme();
     String authority = uri.getAuthority();
 
-    conf = ConfigurationService.updateConfigurationWithNameService(uri, conf);
-
-    if (scheme == null && authority == null) {     // use default FS
+    if (scheme == null && authority == null) { // use default FS
       return get(conf);
     }
 
-    if (scheme != null && authority == null) {     // no authority
+    if (scheme != null && authority == null) { // no authority
       URI defaultUri = getDefaultUri(conf);
-      if (scheme.equals(defaultUri.getScheme())    // if scheme matches default
-              && defaultUri.getAuthority() != null) {  // & default has authority
-        return get(defaultUri, conf);              // return default
+      if (scheme.equals(defaultUri.getScheme()) // if scheme matches default
+          && defaultUri.getAuthority() != null) { // & default has authority
+        return get(defaultUri, conf); // return default
       }
     }
 
     String disableCacheName = String.format("fs.%s.impl.disable.cache", scheme);
     if (conf.getBoolean(disableCacheName, false)) {
-      return createFileSystem(uri, conf);
+      createFileSystemWithConfigurationService(uri, conf);
     }
 
     return CACHE.get(uri, conf);
@@ -2436,6 +2433,23 @@ public abstract class FileSystem extends Configured implements Closeable {
     return clazz;
   }
 
+  private static FileSystem createFileSystemWithConfigurationService(URI uri,
+      Configuration conf) throws IOException {
+    return ConfigurationService.createTargetObjWithConfigurationService(
+        new ConfigurationService.Creator<FileSystem, IOException>() {
+          @Override
+          public FileSystem create(URI uri, Configuration configuration,
+              Object... params) throws IOException {
+            return createFileSystem(uri, configuration);
+          }
+
+          @Override
+          public boolean retryWithConfigurationService(Exception e) {
+            return e instanceof java.lang.IllegalArgumentException;
+          }
+        }, uri, conf);
+  }
+
   private static FileSystem createFileSystem(URI uri, Configuration conf
   ) throws IOException {
     Class<?> clazz = getFileSystemClass(uri.getScheme(), conf);
@@ -2489,7 +2503,7 @@ public abstract class FileSystem extends Configured implements Closeable {
         return fs;
       }
 
-      fs = createFileSystem(uri, conf);
+      fs = createFileSystemWithConfigurationService(uri, conf);
       synchronized (this) { // refetch the lock again
         FileSystem oldfs = map.get(key);
         if (oldfs != null) { // a file system is created while lock is releasing
