fa7b7cee6c01f141df5795cc79edf98edf2b9d18
lijinglun [李经纶] <lijinglun@xiaomi.com>
2017-10-19 17:18:12 +0800

    ConfigurationService supports RegisterCenter(NameService)
    Summary:
    Ref T9230
    Now ConfigurationService supports getting conf from both zk and RegisterCenter
    
    Test Plan: UT
    
    Reviewers: zhangchen5, zhouyingchao, xiegang1, yangyun1, zhangsen
    
    Reviewed By: zhangchen5
    
    Maniphest Tasks: T9230
    
    Differential Revision: https://phabricator.d.xiaomi.net/D69620

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/ZookeeperConfigurationService.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/ZookeeperConfigurationService.java
@@ -3,7 +3,6 @@ package org.apache.hadoop.fs;
 import java.io.ByteArrayInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
-import java.util.Properties;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -13,18 +12,25 @@ import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooKeeper;
 
 public class ZookeeperConfigurationService extends ConfigurationService {
+
+  // ZookeeperConfigurationService config items
+  public static final String CONFIGURATION_SERVICE_ZOOKEEPER_HOST =
+      "configuration.service.zookeeper.host";
+  public static final String CONFIGURATION_SERVICE_ZOOKEEPER_SESSION_TIMEOUT =
+      "configuration.service.zookeeper.sessiontimeout";
+
   public static final Log LOG = LogFactory.getLog(ZookeeperConfigurationService.class);
-  public ZookeeperConfigurationService(Properties prop) {
-    super(prop);
+  public ZookeeperConfigurationService(Configuration conf) {
+    super(conf);
   }
 
   @Override
   public Configuration fetchConfiguration(String authority) throws IOException {
-    String zkHost = prop.getProperty(CONFIGURATION_SERVICE_ZOOKEEPER_HOST);
+    String zkHost = conf.get(CONFIGURATION_SERVICE_ZOOKEEPER_HOST);
     if (zkHost == null) {
       throw new IOException("Couldn't find Zookeeper Host.");
     }
-    String timeout = prop.getProperty(CONFIGURATION_SERVICE_SESSION_TIMEOUT);
+    String timeout = conf.get(CONFIGURATION_SERVICE_ZOOKEEPER_SESSION_TIMEOUT);
     int sessionTimeout = timeout == null ? 3000 : Integer.parseInt(timeout);
     return fetchConfFromZk(zkHost, sessionTimeout, authority);
   }
@@ -56,14 +62,6 @@ public class ZookeeperConfigurationService extends ConfigurationService {
     }
 
     return createConfFromBytes(bytes);
-  }
-
-  private Configuration createConfFromBytes(byte[] bytes) throws IOException {
-    Configuration conf = new Configuration(false);
-    ByteArrayInputStream in = new ByteArrayInputStream(bytes);
-    conf.readFields(new DataInputStream(in));
-    in.close();
-    return conf;
   }
 
 }
