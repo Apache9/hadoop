6a8baeb82a13a9e1bb0f1245def1e46334a6a90f
Xie Gang <xiegang1@xiaomi.com>
2018-03-28 16:35:04 +0800

    DOOP-12975 Add jitter to CachingGetSpaceUsed's thread
    Summary: Ref T10447
    
    Test Plan: UT
    
    Reviewers: zhouyingchao, zhangchen5, yangyun1, lijinglun
    
    Reviewed By: zhangchen5
    
    Maniphest Tasks: T10447
    
    Differential Revision: https://phabricator.d.xiaomi.net/D89047

--- hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/DU.java
+++ hadoop-common-project/hadoop-common/src/main/java/org/apache/hadoop/fs/DU.java
@@ -34,13 +34,17 @@ public class DU extends CachingGetSpaceUsed {
   private final DUShell duShell;
 
   @VisibleForTesting
-   public DU(File path, long interval, long initialUsed) throws IOException {
-    super(path, interval, initialUsed);
+  public DU(File path, long interval, long jitter, long initialUsed)
+      throws IOException {
+    super(path, interval, jitter, initialUsed);
     this.duShell = new DUShell();
   }
 
   public DU(CachingGetSpaceUsed.Builder builder) throws IOException {
-    this(builder.getPath(), builder.getInterval(), builder.getInitialUsed());
+    this(builder.getPath(),
+        builder.getInterval(),
+        builder.getJitter(),
+        builder.getInitialUsed());
   }
 
   @Override
