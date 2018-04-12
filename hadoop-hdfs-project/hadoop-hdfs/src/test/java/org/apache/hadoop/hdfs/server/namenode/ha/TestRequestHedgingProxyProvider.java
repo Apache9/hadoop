Apache9/hadoop

commit 3d48630f64df0afd8fb18ae9a3a24ee06e341c93

fix RequestHedgingProxyProvider bug
Summary: RequestHedgingProxyProvider always request all NameNodes even we already got a successful one.

Test Plan: UT

Reviewers: zhangchen5, xiegang1, zhouyingchao, yangyun1, zhangsen

Reviewed By: zhangchen5

Differential Revision: https://phabricator.d.xiaomi.net/D89476

hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ha/RequestHedgingProxyProvider.java
@@ -30,6 +30,7 @@ import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.locks.ReentrantLock;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -80,6 +81,9 @@ public class RequestHedgingProxyProvider<T> extends
     public Object
     invoke(Object proxy, final Method method, final Object[] args)
             throws Throwable {
+      if (successfulProxy != null) {
+        return method.invoke(successfulProxy.proxy,args);
+      }
       if (targetProxies.isEmpty()) {
         if (tolerateNoNn) {
           // will trigger failover

hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestRequestHedgingProxyProvider.java
@@ -32,6 +32,7 @@ import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hdfs.DFSConfigKeys;
 import org.apache.hadoop.hdfs.server.namenode.ha.ConfiguredFailoverProxyProvider.ProxyFactory;
 import org.apache.hadoop.hdfs.server.protocol.NamenodeProtocols;
+import org.apache.hadoop.io.retry.FailoverProxyProvider;
 import org.apache.hadoop.io.retry.MultiException;
 import org.apache.hadoop.ipc.RemoteException;
 import org.apache.hadoop.ipc.StandbyException;
@@ -49,6 +50,8 @@ import org.mockito.invocation.InvocationOnMock;
 import org.mockito.stubbing.Answer;
 
 import com.google.common.collect.Lists;
+
+import static junit.framework.TestCase.assertTrue;
 
 public class TestRequestHedgingProxyProvider {
 
@@ -75,6 +78,39 @@ public class TestRequestHedgingProxyProvider {
     conf.set(
         DFSConfigKeys.DFS_NAMENODE_RPC_ADDRESS_KEY + "." + ns + ".nn2",
         "machine2.foo.bar:8020");
+  }
+
+  @Test
+  public void testHedgingWhenOneSuccess() throws Exception {
+    final AtomicInteger count = new AtomicInteger(0);
+    // good Mock
+    final NamenodeProtocols goodMock = Mockito.mock(NamenodeProtocols.class);
+    Mockito.when(goodMock.getStats()).thenAnswer(new Answer<long[]>() {
+      @Override
+      public long[] answer(InvocationOnMock invocation) throws Throwable {
+        count.incrementAndGet();
+        Thread.sleep(1000);// sleep so bad mock could be called.
+        return new long[]{1};
+      }
+    });
+    // bad Mock
+    final NamenodeProtocols badMock = Mockito.mock(NamenodeProtocols.class);
+    Mockito.when(badMock.getStats()).thenAnswer(new Answer<long[]>() {
+      @Override
+      public long[] answer(InvocationOnMock invocation) throws Throwable {
+        count.incrementAndGet();
+        throw new IOException("Bad Mock! This is Standby!");
+      }
+    });
+
+    RequestHedgingProxyProvider<NamenodeProtocols> provider =
+        new RequestHedgingProxyProvider<>(conf, nnUri, NamenodeProtocols.class,
+            createFactory(badMock,goodMock,goodMock,badMock));
+    NamenodeProtocols proxy = provider.getProxy().proxy;
+    long[] stats = proxy.getStats();
+    assertTrue(count.get()==2);
+    proxy.getStats();
+    assertTrue(count.get()==3);
   }
 
   @Test

commit 32e39d949f8b5ea012ccfb4b17d990581855234d

fix RequestHedgingHandler throws wrong Exception
Summary: fix RequestHedgingHandler throws wrong Exception. The bug is introduced by D89476.

Test Plan: Unit Test

Reviewers: zhangchen5, xiegang1, zhouyingchao, yangyun1, zhangsen

Reviewed By: yangyun1

Differential Revision: https://phabricator.d.xiaomi.net/D90818

hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/server/namenode/ha/RequestHedgingProxyProvider.java
@@ -18,6 +18,7 @@
 package org.apache.hadoop.hdfs.server.namenode.ha;
 
 import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.net.URI;
@@ -26,14 +27,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.concurrent.CompletionService;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorCompletionService;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
-import java.util.concurrent.locks.ReentrantLock;
 
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.ipc.RemoteException;
 import org.apache.hadoop.ipc.StandbyException;
@@ -81,8 +80,19 @@ public class RequestHedgingProxyProvider<T> extends
     public Object
     invoke(Object proxy, final Method method, final Object[] args)
             throws Throwable {
-      if (successfulProxy != null) {
-        return method.invoke(successfulProxy.proxy,args);
+      if (currentUsedProxy != null) {
+        try {
+          Object retVal = method.invoke(currentUsedProxy.proxy, args);
+          LOG.debug("Invocation successful on [{}]",
+              currentUsedProxy.proxyInfo);
+          return retVal;
+        } catch (InvocationTargetException ex) {
+          Exception unwrappedException = unwrapInvocationTargetException(ex);
+          logProxyException(unwrappedException, currentUsedProxy.proxyInfo);
+          LOG.trace("Unsuccessful invocation on [{}]",
+              currentUsedProxy.proxyInfo);
+          throw unwrappedException;
+        }
       }
       if (targetProxies.isEmpty()) {
         if (tolerateNoNn) {
@@ -121,14 +131,15 @@ public class RequestHedgingProxyProvider<T> extends
           Object retVal = null;
           try {
             retVal = callResultFuture.get();
-            successfulProxy = proxyMap.get(callResultFuture);
+            currentUsedProxy = proxyMap.get(callResultFuture);
             LOG.debug("Invocation successful on [{}]",
-                successfulProxy.proxyInfo);
+                currentUsedProxy.proxyInfo);
             return retVal;
-          } catch (Exception ex) {
+          } catch (ExecutionException ex) {
+            Exception unwrappedException = unwrapExecutionException(ex);
             ProxyInfo<T> tProxyInfo = proxyMap.get(callResultFuture);
-            logProxyException(ex, tProxyInfo.proxyInfo);
-            badResults.put(tProxyInfo.proxyInfo, unwrapException(ex));
+            logProxyException(unwrappedException, tProxyInfo.proxyInfo);
+            badResults.put(tProxyInfo.proxyInfo, unwrappedException);
             LOG.trace("Unsuccessful invocation on [{}]", tProxyInfo.proxyInfo);
             numAttempts--;
           }
@@ -151,7 +162,7 @@ public class RequestHedgingProxyProvider<T> extends
   }
 
 
-  private volatile ProxyInfo<T> successfulProxy = null;
+  private volatile ProxyInfo<T> currentUsedProxy = null;
 
   public RequestHedgingProxyProvider(
           Configuration conf, URI uri, Class<T> xface) {
@@ -167,8 +178,8 @@ public class RequestHedgingProxyProvider<T> extends
   @SuppressWarnings("unchecked")
   @Override
   public synchronized ProxyInfo<T> getProxy() {
-    if (successfulProxy != null) {
-      return successfulProxy;
+    if (currentUsedProxy != null) {
+      return currentUsedProxy;
     }
     Map<String, ProxyInfo<T>> targetProxyInfos = new HashMap<>();
     StringBuilder combinedInfo = new StringBuilder("[");
@@ -188,7 +199,7 @@ public class RequestHedgingProxyProvider<T> extends
 
   @Override
   public synchronized void performFailover(T currentProxy) {
-    successfulProxy = null;
+    currentUsedProxy = null;
   }
 
   /**
@@ -199,9 +210,9 @@ public class RequestHedgingProxyProvider<T> extends
    */
   private void logProxyException(Exception ex, String proxyInfo) {
     if (isStandbyException(ex)) {
-      LOG.debug("Invocation returned standby exception on [{}]", proxyInfo);
+      LOG.debug("Invocation returned standby exception on [{}]", proxyInfo, ex);
     } else {
-      LOG.warn("Invocation returned exception on [{}]", proxyInfo);
+      LOG.warn("Invocation returned exception on [{}]", proxyInfo, ex);
     }
   }
 
@@ -210,8 +221,7 @@ public class RequestHedgingProxyProvider<T> extends
    * @param ex Exception to check.
    * @return If the exception is caused by an standby namenode.
    */
-  private boolean isStandbyException(Exception ex) {
-    Exception exception = unwrapException(ex);
+  private boolean isStandbyException(Exception exception) {
     if (exception instanceof RemoteException) {
       return ((RemoteException) exception).unwrapRemoteException()
           instanceof StandbyException;
@@ -220,24 +230,43 @@ public class RequestHedgingProxyProvider<T> extends
   }
 
   /**
-   * Unwraps the exception. <p>
+   * Unwraps the ExecutionException. <p>
    * Example:
    * <blockquote><pre>
    * if ex is
-   * ExecutionException(InvocationTargetExeption(SomeException))
+   * ExecutionException(InvocationTargetException(SomeException))
    * returns SomeException
    * </pre></blockquote>
    *
    * @return unwrapped exception
    */
-  private Exception unwrapException(Exception ex) {
+  private Exception unwrapExecutionException(ExecutionException ex) {
+    if (ex != null) {
+      Throwable cause = ex.getCause();
+      if (cause instanceof InvocationTargetException) {
+        return
+            unwrapInvocationTargetException((InvocationTargetException)cause);
+      }
+    }
+    return ex;
+
+  }
+
+  /**
+   * Unwraps the InvocationTargetException. <p>
+   * Example:
+   * <blockquote><pre>
+   * if ex is InvocationTargetException(SomeException)
+   * returns SomeException
+   * </pre></blockquote>
+   *
+   * @return unwrapped exception
+   */
+  private Exception unwrapInvocationTargetException(
+      InvocationTargetException ex) {
     if (ex != null) {
       Throwable cause = ex.getCause();
       if (cause instanceof Exception) {
-        Throwable innerCause = cause.getCause();
-        if (innerCause instanceof Exception) {
-          return (Exception) innerCause;
-        }
         return (Exception) cause;
       }
     }

hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/namenode/ha/TestRequestHedgingProxyProvider.java
@@ -51,6 +51,7 @@ import org.mockito.stubbing.Answer;
 
 import com.google.common.collect.Lists;
 
+import static junit.framework.TestCase.assertEquals;
 import static junit.framework.TestCase.assertTrue;
 
 public class TestRequestHedgingProxyProvider {
@@ -111,6 +112,45 @@ public class TestRequestHedgingProxyProvider {
     assertTrue(count.get()==2);
     proxy.getStats();
     assertTrue(count.get()==3);
+  }
+
+  @Test
+  public void testExceptionInfo() throws Exception {
+    final NamenodeProtocols goodMock = Mockito.mock(NamenodeProtocols.class);
+    Mockito.when(goodMock.getStats()).thenAnswer(new Answer<long[]>() {
+      boolean first = true;
+      @Override
+      public long[] answer(InvocationOnMock invocation)
+          throws Throwable {
+        if (first) {
+          Thread.sleep(1000);// sleep so bad mock could be called.
+          first = false;
+          return new long[] { 1 };
+        } else {
+          throw new IOException("Expected Exception Info");
+        }
+      }
+    });
+    final NamenodeProtocols badMock = Mockito.mock(NamenodeProtocols.class);
+    Mockito.when(badMock.getStats()).thenAnswer(new Answer<long[]>() {
+      @Override
+      public long[] answer(InvocationOnMock invocation)
+          throws Throwable {
+        throw new IOException("Bad Mock! This is Standby!");
+      }
+    });
+
+    RequestHedgingProxyProvider<NamenodeProtocols> provider =
+        new RequestHedgingProxyProvider<>(conf, nnUri, NamenodeProtocols.class,
+            createFactory(badMock, goodMock));
+    NamenodeProtocols proxy = provider.getProxy().proxy;
+    proxy.getStats();
+    try {
+      proxy.getStats();
+    } catch (Exception e) {
+      assertTrue(e instanceof IOException);
+      assertEquals("Expected Exception Info", e.getMessage());
+    }
   }
 
   @Test