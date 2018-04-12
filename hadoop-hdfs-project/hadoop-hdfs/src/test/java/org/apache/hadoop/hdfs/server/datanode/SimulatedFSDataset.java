00a238a927f3ed8c3d6fd12cacbc3f5cb0f817f8
zhangchen <zhangchen5@xiaomi.com>
2018-04-12 15:36:33 +0800

    reportBadBlock when DataNode#transferBlock encounter EIO error
    Summary: Ref T10447
    
    Test Plan: UT
    
    Reviewers: zhouyingchao, xiegang1, yangyun1, lijinglun, sunlisheng, wangyuxuan
    
    Reviewed By: xiegang1
    
    Maniphest Tasks: T10447
    
    Differential Revision: https://phabricator.d.xiaomi.net/D90881

--- hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/SimulatedFSDataset.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/test/java/org/apache/hadoop/hdfs/server/datanode/SimulatedFSDataset.java
@@ -466,7 +466,7 @@ public class SimulatedFSDataset implements FsDatasetSpi<FsVolumeSpi> {
 
     @Override
     public StorageType getStorageType() {
-      return null;
+      return StorageType.DISK;
     }
 
     @Override
@@ -874,7 +874,7 @@ public class SimulatedFSDataset implements FsDatasetSpi<FsVolumeSpi> {
     return binfo;
   }
 
-  synchronized InputStream getBlockInputStream(ExtendedBlock b
+  public synchronized InputStream getBlockInputStream(ExtendedBlock b
       ) throws IOException {
     final Map<Block, BInfo> map = getMap(b.getBlockPoolId());
     BInfo binfo = map.get(b.getLocalBlock());
