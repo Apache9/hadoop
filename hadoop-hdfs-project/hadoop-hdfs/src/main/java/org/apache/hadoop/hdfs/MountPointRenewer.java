6a84482de91d79c41515eb53e92d821edac41c67
zhangchen <zhangchen5@xiaomi.com>
2018-05-16 20:44:34 +0800

    update MP renewer, update namespace configs to znode
    Summary: Ref T11355
    
    Test Plan: e2e
    
    Reviewers: #hdfs-zk, lijinglun
    
    Reviewed By: #hdfs-zk, lijinglun
    
    Subscribers: lijinglun
    
    Maniphest Tasks: T11355
    
    Differential Revision: https://phabricator.d.xiaomi.net/D95271

--- hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/MountPointRenewer.java
+++ hadoop-hdfs-project/hadoop-hdfs/src/main/java/org/apache/hadoop/hdfs/MountPointRenewer.java
@@ -1,17 +1,25 @@
 package org.apache.hadoop.hdfs;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
+import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 import java.util.Map.Entry;
 
+import com.google.common.base.Joiner;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.CommonConfigurationKeys;
 import org.apache.hadoop.fs.viewfs.Constants;
-import org.apache.hadoop.hdfs.server.namenode.FSNamesystem;
+import org.apache.hadoop.hdfs.server.namenode.ha.ZkConfiguredFailoverProxyProvider;
 import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
@@ -64,8 +72,9 @@ public class MountPointRenewer {
    * format.
    */
   public static String getMountPointConfig(final Configuration config,
-      final String viewName) throws IOException {
+      final String viewName, final boolean includeNsConfig) throws IOException {
     StringBuilder mpBuilder = new StringBuilder();
+    Set<String> nsIds = new HashSet<String>();
     String vName = viewName;
     if (vName == null) {
       vName = Constants.CONFIG_VIEWFS_DEFAULT_MOUNT_TABLE;
@@ -97,16 +106,80 @@ public class MountPointRenewer {
         if (!isFirstEntry) {
           mpBuilder.append(";");
         }
+
+        if (includeNsConfig) {
+          String nsId;
+          try {
+            nsId = new URI(target).getAuthority();
+          } catch (URISyntaxException use) {
+            throw new IOException(use);
+          }
+          if (nsId != null && !nsIds.contains(nsId)) {
+            String nsConfig = getNsConfigs(config, nsId);
+            if (nsConfig != null) {
+              nsIds.add(nsId);
+              mpBuilder.append(nsConfig);
+            }
+          }
+        }
+
         mpBuilder.append(si.getKey()).append("=").append(target);
         isFirstEntry = false;
       }
     }
+    if (includeNsConfig) {
+      if (!isFirstEntry) {
+        mpBuilder.append(";");
+      }
+      mpBuilder.append(DFSConfigKeys.DFS_NAMESERVICES).append("=")
+              .append(Joiner.on(",").skipNulls().join(nsIds));
+    }
+
     return mpBuilder.toString();
+  }
+
+  public static String getNsConfigs(Configuration conf, String nsId)
+      throws IOException {
+    if (nsId.contains(":")) { // it's an address
+      return null;
+    }
+
+    StringBuilder builder = new StringBuilder();
+    // add failover provider config
+    String failoverProviderKey = DFSUtil.addKeySuffixes(
+        DFSConfigKeys.DFS_CLIENT_FAILOVER_PROXY_PROVIDER_KEY_PREFIX, nsId);
+    String failoverClassName = conf.get(failoverProviderKey,
+        ZkConfiguredFailoverProxyProvider.class.getName());
+    builder.append(failoverProviderKey).append("=").append(failoverClassName);
+    builder.append(";");
+
+    // add dfs.ha.namenodes config
+    String nsIdKey =
+        DFSUtil.addKeySuffixes(DFSConfigKeys.DFS_HA_NAMENODES_KEY_PREFIX, nsId);
+    Collection<String> nnIds = DFSUtil.getNameNodeIds(conf, nsId);
+    if (nnIds.size() != 2) {
+      throw new IOException("Can't get HA configs for nsId: " + nsId);
+    }
+    builder.append(nsIdKey).append("=").append(conf.get(nsIdKey));
+    builder.append(";");
+
+    // add rpc-addresses
+    for (String nnId : nnIds) {
+      String addrKey = DFSUtil.addKeySuffixes(
+          DFSConfigKeys.DFS_NAMENODE_RPC_ADDRESS_KEY, nsId, nnId);
+      String NNAddr = conf.get(addrKey);
+      if (NNAddr == null) {
+        throw new IOException("Can't get NN address configs for " + addrKey);
+      }
+      builder.append(addrKey).append("=").append(NNAddr);
+      builder.append(";");
+    }
+    return builder.toString();
   }
 
   public static void verifyNewMountPoints(Configuration conf, String kvConf,
       String viewName) throws IllegalArgumentException, IOException {
-    String origKvConfig = getMountPointConfig(conf, viewName);
+    String origKvConfig = getMountPointConfig(conf, viewName, false);
     String[] origKvs = origKvConfig.split(";");
     String[] kvs = kvConf.split(";");
     for (String origKv : origKvs) {
@@ -141,6 +214,12 @@ public class MountPointRenewer {
       if (conf.get(key) == null) {
         res = true;
         conf.set(key, val);
+      } else if (key.equals(DFSConfigKeys.DFS_NAMESERVICES)) {
+        res = true;
+        Set<String> nsSet = new HashSet<String>();
+        nsSet.addAll(Arrays.asList(conf.get(key).split(",")));
+        nsSet.addAll(Arrays.asList(val.split(",")));
+        conf.set(key, Joiner.on(",").skipNulls().join(nsSet));
       }
     }
     return res;
@@ -226,7 +305,7 @@ public class MountPointRenewer {
   }
 
   public ZooKeeper getZkClient() throws IOException {
-    String zkQuorum = conf.get(CommonConfigurationKeys.ZK_QUORUM_KEY);
+    String zkQuorum = getZkQuorum(conf);
     if (zkQuorum == null) {
       return null;
     }
